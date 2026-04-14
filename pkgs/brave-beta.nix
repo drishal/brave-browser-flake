4fbd10c1a8b101bb5aa4c191aefc9bf2efa64049
kcalvelli <1329446+kcalvelli@users.noreply.github.com>
2026-04-14 04:54:18 +0000

    chore: update brave versions

--- pkgs/brave-beta.nix
+++ pkgs/brave-beta.nix
@@ -1,7 +1,7 @@
 { callPackage, fetchurl }:
 let
-  version = "1.90.102";
-  hash = "1r0vfg6zky3r2k8mxin95clrmbmwyy410nknj8c8q4hjrl6ig1iy";
+  version = "1.90.103";
+  hash = "0lj963s8ka9z9wih04lnxqp66l1pp807il5ih0mwaxqwl84x32ma";
 in
 callPackage ./build-brave.nix { } {
   pname = "brave-beta";
