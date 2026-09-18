659ac93d0b389d13f0d80b5fc8e073a1f8fdc2b4
drishal <32875739+drishal@users.noreply.github.com>
2026-09-18 10:01:05 +0000

    chore: update brave versions — brave-nightly 1.98.8 — brave-beta 1.97.43 — brave-stable 1.95.104

--- pkgs/brave-stable.nix
+++ pkgs/brave-stable.nix
@@ -1,10 +1,10 @@
 { callPackage, fetchurl }:
 let
-  version = "1.95.102";
-  hash = "1zmx3227h4y4pq4brd71lf1w1knbxbll7ac8fcrf6fy8azz100ck";
+  version = "1.95.104";
+  hash = "0vj2fsr0c8hdaf85fas099s0fjbw1j1vx828a3k7aky8kwz3r7p0";
 in
 callPackage ./build-brave.nix { } {
   pname = "brave-stable";
   inherit version hash;
-  url = "https://github.com/brave/brave-browser/releases/download/v1.95.102/brave-browser_1.95.102_amd64.deb";
+  url = "https://github.com/brave/brave-browser/releases/download/v1.95.104/brave-browser_1.95.104_amd64.deb";
 }
