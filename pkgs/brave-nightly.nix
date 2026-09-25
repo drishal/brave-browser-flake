26293fba53b3d5a2345d9b19d7b7d0b6a10f0414
drishal <32875739+drishal@users.noreply.github.com>
2026-09-25 10:28:31 +0000

    chore: update brave versions — brave-nightly 1.98.30 — brave-origin-nightly 1.98.28

--- pkgs/brave-nightly.nix
+++ pkgs/brave-nightly.nix
@@ -1,11 +1,11 @@
 { callPackage, fetchurl }:
 let
-  version = "1.98.27";
-  hash = "1bnhac873035ijq0mlwa1y1gxvmxbcy7kwn6d2dpj82c3sdv487h";
+  version = "1.98.30";
+  hash = "0v73p13jspiyp89y100f68p67h63bai8z1c1qn42i3719z5y8v3w";
 in
 callPackage ./build-brave.nix { } {
   pname = "brave-nightly";
   inherit version hash;
-  url = "https://github.com/brave/brave-browser/releases/download/v1.98.27/brave-browser-nightly_1.98.27_amd64.deb";
+  url = "https://github.com/brave/brave-browser/releases/download/v1.98.30/brave-browser-nightly_1.98.30_amd64.deb";
   commandLineArgs = "--enable-features=BraveAIChatAgentProfile";
 }
